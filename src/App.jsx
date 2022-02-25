addb3b83f7bee9fb1deba55b48fd55152a5f837a
Karen <karenmoncada80@gmail.com>
2022-02-25 18:49:54 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+import NavBar from "./NavBar"
+
+function App (){
+    return <>
+    <NavBar/>
+    </>
+}
+
+export default App
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,10 @@
+function NavBar() {
+    return <header>
+        <h1>E-COMMERCE</h1>
+        <a href="">Productos</a>
+        <a href="">SALE</a>
+        <a href="">Contacto</a>
+    </header>
+}
+
+export default NavBar
