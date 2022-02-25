647eafa8dc06bee2662caa38aec417268903471a
Karen <karenmoncada80@gmail.com>
2022-02-25 20:10:46 -0300

    Resolucion de problemas previos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import NavBar from "./NavBar"
+import NavBar from "../src/Components/NavBar"
 
 function App (){
     return <>
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,9 +1,12 @@
 function NavBar() {
     return <header>
         <h1>E-COMMERCE</h1>
+        <nav>
         <a href="">Productos</a>
         <a href="">SALE</a>
         <a href="">Contacto</a>
+        </nav>
+        
     </header>
 }
 
