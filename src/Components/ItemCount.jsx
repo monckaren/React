dea4dd2470ba9d006b022d515fb397960ea18b99
Karen <karenmoncada80@gmail.com>
2022-03-21 23:24:43 -0300

    Adelanto, falta una funcionalidad

--- src/Components/ItemCount.jsx
+++ src/Components/ItemCount.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import {ButtonGroup, Button, InputGroup} from 'react-bootstrap'
 
+
 function ItemCount (props){
     const [contador,setContador] = useState(props.initial)
 
@@ -18,6 +19,17 @@ function ItemCount (props){
     setContador(props.stock)
         
     }
+    const [seleccionado, setSeleccionado] = useState(false)
+    const onAdd = (unidadSeleccionada) => {
+      console.log("desde el itenDetail")
+      if(unidadSeleccionada != undefined){
+      setSeleccionado(unidadSeleccionada)
+      }
+      }
+      const handleOnAdd = () => {
+        console.log("on add")
+        onAdd(contador)
+      } 
     return (
     <>
     <ButtonGroup aria-label="Basic example">
@@ -28,7 +40,7 @@ function ItemCount (props){
     </InputGroup>
     <Button variant="secondary" onClick={handleClick}> + </Button>
     </ButtonGroup>
-    <Button variant="primary" size="md" active onClick={props.onAdd}>
+    <Button variant="primary" size="md" active onClick={handleOnAdd}>
     Comprar
   </Button>
     <p> Has agregado al carrito {contador} producto (s) </p>
--- src/Components/ItemDetail.jsx
+++ src/Components/ItemDetail.jsx
@@ -1,9 +1,12 @@
 import {Card} from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import ItemCount from './ItemCount'
- const handleOnAdd = () => {
-   console.log("on add")
- } 
+import { useState } from 'react'
+
+ 
 const ItemDetail = ({item}) => {
+
+  const [seleccionado, setSeleccionado] = useState(false)
     return <>
     <div className='detailCard mt-5'>
     <Card style={{ width: '25rem' }}>
@@ -11,8 +14,9 @@ const ItemDetail = ({item}) => {
   <Card.Body>
   <Card.Title>{item[0].nombre}</Card.Title>
   <Card.Text>{item[0].descripcion}</Card.Text>
-  <ItemCount initial={1} stock={10} onAdd={handleOnAdd}/>
-  </Card.Body>
+  <ItemCount initial={1} stock={10} />
+  <p>{seleccionado ? "ya se selecciono algo!" : "No se eligio ninguna cantidad"}</p>
+  {seleccionado ? <Link to="/carrito">carrito</Link> : null}</Card.Body>
   </Card>
     </div>
     </>
